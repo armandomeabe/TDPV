80043b11538f01d8c1a8351ce605f410aaa5d4c7
Armando Andrés Meabe <armando.meabe@gmail.com>
2024-10-08 01:51:44 -0300

    proyecto de ejemplo con SFML funcionando
    proyecto de ejemplo con SFML funcionando

---
+++ Mavi/MAVISFML/MAVISFML/MAVISFML.cpp
@@ -0,0 +1,21 @@
+//////Bibliotecas//////
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+///Punto de entrada a la aplicación///
+
+
+int main() {
+	//Creamos la ventana
+	sf::RenderWindow App(sf::VideoMode(800, 600, 32),
+		"Que ventana horrible");
+	// Loop principal
+	while (App.isOpen())
+	{
+		// Limpiamos la ventana
+		App.clear();
+		//Dibujamos la escena
+		 // Mostramos la ventana
+		App.display();
+	}
+	return 0;
+}
