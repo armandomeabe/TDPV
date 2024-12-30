2123fb830f1d0f726ccde3978a75be3bd407226f
Armando Andrés Meabe <armando.meabe@gmail.com>
2024-12-30 01:15:41 -0300

    armando el framework

--- Mavi/Proyectos/Proyecto Final MAVI/MAVISFML/GameManager.cpp
+++ Mavi/Proyectos/Proyecto Final MAVI/MAVISFML/GameManager.cpp
@@ -1,1 +1,45 @@
 #include "GameManager.h"
+
+GameManager::GameManager(sf::RenderWindow* app) : App(app)
+{
+	App->setMouseCursorVisible(false); // Ocultar el cursor del mouse
+}
+
+void GameManager::Start()
+{
+	// Reiniciar el reloj del tiempo total y de actualización
+	gameTimeClock.restart();
+	updateClock.restart();
+
+	// Comenzar el ciclo del juego
+	while (App->isOpen())
+	{
+		HandleEvents();
+		Update();
+	}
+}
+
+void GameManager::HandleEvents()
+{
+	sf::Event event;
+	while (App->pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+		{
+			App->close();
+		}
+		else if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+		{
+			// en caso de click... etc.
+		}
+	}
+}
+
+void GameManager::Update()
+{
+	App->clear();
+	
+	// Logica aca...
+
+	App->display();
+}
--- Mavi/Proyectos/Proyecto Final MAVI/MAVISFML/GameManager.h
+++ Mavi/Proyectos/Proyecto Final MAVI/MAVISFML/GameManager.h
@@ -1,5 +1,20 @@
 #pragma once
+
+#include "Includes.h"
+
 class GameManager
 {
+public:
+    GameManager(sf::RenderWindow* app);
+    void Start();
+    void HandleEvents();
+    void Update();
+
+private:
+    sf::RenderWindow* App;
+
+    // Control de tiempo
+    sf::Clock updateClock;   // Para actualizaciones cada 5 segundos
+    sf::Clock gameTimeClock; // Para medir el tiempo total de juego
 };
 
